da0e1cb83518e7ceab9da5d4b5ec20c75389f213
swayamrajora <127737419+swayamrajora@users.noreply.github.com>
2025-05-12 10:31:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    const charCount = {};
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+    for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+    return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+console.log(firstNonRepeatedChar('aabbcdd'));
